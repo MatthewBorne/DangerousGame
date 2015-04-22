aea32016b5c2f5487fbba313a064a7410ed91896
Eric Zeringue <ezchess9@yahoo.com>
2015-04-21 19:03:59 -0500

    fixed gunshot scene more
    fixed bugs

--- animations.js
+++ animations.js
@@ -579,11 +579,13 @@ $(document).ready(function(){
     twnGargoyles.add(   [TweenMax.to("#imgGunPoint", .4, {opacity: 0}),
                          TweenMax.to("#imgGunPoint2", .4, {opacity: 1})]);
 						 
-	twnGargoyles.add([TweenMax.to("#imgGunPoint2", .4, {opacity: 0}),
-                         TweenMax.to("#imgYellowGunshot", .4, {opacity: 1})]);
+	/*twnGargoyles.add([TweenMax.to("#imgGunPoint2", .4, {opacity: 0}),
+                         TweenMax.to("#imgYellowGunshot", .4, {opacity: 1})]);*/
 						 
 	twnGargoyles.add(TweenMax.to("#imgEasyIvanText", 2, {opacity: 1}));
 	twnGargoyles.add(TweenMax.to("#imgEasyIvanText", 2, {opacity: 0}));
+	
+	
 
     twnGargoyles.add(   [TweenMax.to("#imgGunPoint2", .2, {opacity: 0}),
                          TweenMax.to("#imgHandShake", .2, {opacity: 1})]);
